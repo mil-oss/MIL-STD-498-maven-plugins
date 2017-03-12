9cf6a2437182a89772bf320a0b7eea38f0a890bf
Alex O'Ree <alexoree@apache.org>
2017-03-11 23:08:24 -0500

    license info

--- vdd-maven-plugin/src/main/java/org/miloss/github/Asset.java
+++ vdd-maven-plugin/src/main/java/org/miloss/github/Asset.java
@@ -1,4 +1,16 @@
-
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.miloss.github;
 
 import java.io.Serializable;
@@ -26,8 +38,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
     "updated_at",
     "browser_download_url"
 })
-public class Asset implements Serializable
-{
+public class Asset implements Serializable {
 
     @JsonProperty("url")
     private String url;
